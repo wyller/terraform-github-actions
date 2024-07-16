5162dad27501373e0a8764660c6cb3e2a7ff3fd6
wyller <11397803+wyller@users.noreply.github.com>
2024-07-16 19:58:50 -0300

    feature: mudando nome do as

--- variables.tf
+++ variables.tf
@@ -19,24 +19,5 @@ variable "vm_username" {
 variable "vm_password" {
   type        = string
   description = "vm password"
-}
-
-variable "az_subscription_id" {
-  type        = string
-  description = "azure subscription id"
-}
-
-variable "az_tenant_id" {
-  type        = string
-  description = "azure tenant id"
-}
-
-variable "az_client_id" {
-  type        = string
-  description = "azure client id"
-}
-
-variable "az_client_secret" {
-  type        = string
-  description = "azure clinet secret"
+  default     = "admin@123"
 }
