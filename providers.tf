b7c60ee6c6d6e751eed83ec9b331455dc4d8daae
wyller <11397803+wyller@users.noreply.github.com>
2024-07-16 19:07:30 -0300

    feature: mudando nome do as

--- providers.tf
+++ providers.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "student-rg"
-    storage_account_name = "studentrg'"
+    resource_group_name  = "student-storage-rg"
+    storage_account_name = "studentstorage777'"
     container_name       = "terraform-states"
     key                  = "terraform.tfstate"
   }
@@ -18,4 +18,8 @@ provider "azurerm" {
   features {
 
   }
+  subscription_id = var.az_subscription_id
+  tenant_id       = var.az_tenant_id
+  client_id       = var.az_client_id
+  client_secret   = var.az_client_secret
 }
--- variables.tf
+++ variables.tf
@@ -21,3 +21,22 @@ variable "vm_password" {
   description = "vm password"
 }
 
+variable "az_subscription_id" {
+  type        = string
+  description = "azure subscription id"
+}
+
+variable "az_tenant_id" {
+  type        = string
+  description = "azure tenant id"
+}
+
+variable "az_client_id" {
+  type        = string
+  description = "azure client id"
+}
+
+variable "az_client_secret" {
+  type        = string
+  description = "azure clinet secret"
+}
