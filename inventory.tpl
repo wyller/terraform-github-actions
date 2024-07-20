d359ac2b4f256dbe7b90a6a1906396a3b8e9de0f
wyller <11397803+wyller@users.noreply.github.com>
2024-07-20 08:12:31 -0300

    feature: change access ssh

--- inventory.tpl
+++ inventory.tpl
@@ -1,3 +1,2 @@
 [web]
-${web_ip} ansible_user=${web_user} ansible_ssh_pass=${web_password}
-
+${web_ip} ansible_user=${web_user} ansible_ssh_pass=${web_password} ansible_ssh_common_args='-o StrictHostKeyChecking=no'
--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "AllowSSH"
+    name                       = "SSH"
     priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
